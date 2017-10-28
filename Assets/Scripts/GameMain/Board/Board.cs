487fd4ffd29c19dad63a2be2d5e076f8459d7358
ysm-suzuki <ys443ys443@yahoo.co.jp>
2017-10-28 19:44:32 +0900

    Added RemoveUnit() to Board class.

--- Assets/Scripts/GameMain/Board/Board.cs
+++ Assets/Scripts/GameMain/Board/Board.cs
@@ -9,12 +9,19 @@ namespace GameMain
         private List<Wall> _walls = new List<Wall>();
         private List<Unit> _units = new List<Unit>();
 
+        private List<Wall> _removingWalls = new List<Wall>();
+        private List<Unit> _removingUnits = new List<Unit>();
+
         public void Tick(float delta)
         {
             foreach (var unit in _units)
                 unit.Tick(delta);
 
             SolveMoving();
+
+            foreach (var removnigUnit in _removingUnits)
+                _units.Remove(removnigUnit);
+            _removingUnits.Clear();
         }
 
 
@@ -29,6 +36,10 @@ namespace GameMain
             _units.Add(unit);
         }
 
+        public void RemoveUnit(Unit unit)
+        {
+            _removingUnits.Add(unit);
+        }
 
 
         private void SolveMoving()
