902b1dce4118bb813552d3f2251290313dba147f
ysm-suzuki <ys443ys443@yahoo.co.jp>
2017-11-24 12:55:27 +0900

    Added the master data version classes

---
+++ Assets/Scripts/Common/MasterData/Version/VersionData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using UnityMVC;
+
+public class VersionData
+{
+    public static VersionDataLoader loader
+    {
+        get { return VersionDataLoader.Instance; }
+    }
+
+    public int major;
+    public int minor;
+    public int revision;
+
+    public override string ToString()
+    {
+        return "v" + major + "." + minor + "." + revision;
+    }
+}
---
+++ Assets/Scripts/Common/MasterData/Version/VersionDataLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+using SQLite.Attribute;
+
+using UnityMVC;
+
+public class VersionDataLoader : MasterDataLoader<VersionData>
+{
+    private static VersionDataLoader _instance = null;
+    public static VersionDataLoader Instance
+    {
+        get { return _instance ?? new VersionDataLoader(); }
+    }
+    private VersionDataLoader() { }
+
+    public VersionData Get()
+    {
+        var sqLite = GameMain.SQLite.Instance.connection;
+        var list = sqLite.Table<version_data>();
+        var data = list.FirstOrDefault();
+
+        return new VersionData
+        {
+            major = data.major,
+            minor = data.minor,
+            revision = data.revision,
+        };
+    }
+
+    public class version_data
+    {
+        public int major { get; set; }
+        public int minor { get; set; }
+        public int revision { get; set; }
+    }
+}
